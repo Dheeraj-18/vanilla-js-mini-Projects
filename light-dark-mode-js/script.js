89e8932b458f0d08c4bdb401d9ee8b6b390aa13a
think2code <dheerajsaraswat03@gmail.com>
2025-09-20 09:22:37 +0530

    feat - implement localStorage & reusable code

--- light-dark-mode-js/script.js
+++ light-dark-mode-js/script.js
@@ -2,20 +2,41 @@ const fullDarkModeCheckBox = document.querySelector('#full-dark-mode')
 const containerDarkModeCheckBox = document.querySelector('#contain-dark-mode')
 const Container = document.querySelector('.container')
 
+const isFullDarkMode = JSON.parse(localStorage.getItem('fullDarkModeCheckBox'))
+const isContainerDarkMode = JSON.parse(
+  localStorage.getItem('containerDarkModeCheckBox')
+)
+
+if (isFullDarkMode) {
+  fullDarkModeCheckBox.checked = true
+}
+if (isContainerDarkMode) {
+  containerDarkModeCheckBox.checked = true
+}
+
+changeFullDarkMode()
+changeContainedDarkMode()
+
 fullDarkModeCheckBox.addEventListener('change', () => {
-  if (fullDarkModeCheckBox.checked) {
-    document.body.classList.add('dark')
-  } else {
-    document.body.classList.remove('dark')
-  }
-  containerDarkModeCheckBox.checked = fullDarkModeCheckBox.checked
+  changeFullDarkMode()
   changeContainedDarkMode()
 })
+
+function changeFullDarkMode() {
+  changeTheme(fullDarkModeCheckBox, document.body, 'fullDarkModeCheckBox')
+}
 function changeContainedDarkMode() {
-  if (containerDarkModeCheckBox.checked) {
-    Container.classList.add('dark')
+  changeTheme(containerDarkModeCheckBox, Container, 'containerDarkModeCheckBox')
+}
+
+// Here write a reusable function 
+function changeTheme(checkbox, element, LocalStorageKey) {
+  if (checkbox.checked) {
+    element.classList.add('dark')
+    localStorage.setItem(LocalStorageKey, true)
   } else {
-    Container.classList.remove('dark')
+    element.classList.remove('dark')
+    localStorage.setItem(LocalStorageKey, false)
   }
 }
 containerDarkModeCheckBox.addEventListener('change', changeContainedDarkMode)
